c9938c320f95ba7e8514b75ecf3ae44869f3115e
Whitney <whitneymarkov@outlook.com>
2017-06-02 23:58:50 +0200

    add html, css, js
    Ran through validator

---
+++ interactive-photo-gallery/js/main.js
@@ -0,0 +1,16 @@
+//variables for the input fields and the images
+
+var $textInput = $('#text-input');
+var $images = $('img')
+
+//use keyup event for live feedback and loop
+$textInput.keyup(function(event) {
+    //use .val() method to return an array containing the value of each selected option. Lowercase method
+  var value = $textInput.val().toLowerCase();
+  //show images if the title matches any input strings
+  $images.show();
+  //if input field is not empty and does not match any strings then hide images
+  if (value !== '') {
+    $images.not('[title*="' + value + '"]').hide();
+  }
+});
